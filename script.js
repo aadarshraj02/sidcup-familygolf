71bfe9641f44683037e159da877f3b9a8bb39ad2
aadarshraj02 <aadarshraazz007@gmail.com>
2024-06-24 19:37:36 +0530

    added custom pointer

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+let cursor = document.querySelector("#cursor");
+let cursorBlur = document.querySelector("#cursor-blur");
+
+var timeout;
+function circleSkew() {
+  var xScale = 1;
+  var yScale = 1;
+  var xPrev = 0;
+  var yPrev = 0;
+
+  window.addEventListener("mousemove", (details) => {
+    this.clearTimeout(timeout);
+    xScale = gsap.utils.clamp(0.8, 1.2, details.clientX - xPrev);
+    yScale = gsap.utils.clamp(0.8, 1.2, details.clientY - yPrev);
+    xPrev = details.clientX;
+    yPrev = details.clientY;
+    circleMouseFollow(xScale, yScale);
+    timeout = this.setTimeout(() => {
+      cursor.style.transform = `
+        translate(${details.clientX}px, ${details.clientY}px) scale(1,1)
+        `;
+    }, 100);
+  });
+}
+
+function circleMouseFollow(xScale, yScale) {
+  window.addEventListener("mousemove", (details) => {
+    cursor.style.transform = `
+    translate(${details.clientX}px, ${details.clientY}px) scale(${xScale},${yScale})
+    `;
+  });
+}
+document.addEventListener("mousemove", (details) => {
+  cursorBlur.style.left = details.x - 150 + "px";
+  cursorBlur.style.top = details.y - 150 + "px";
+});
+
 gsap.to("#nav", {
   backgroundColor: "#000",
   duration: 0.5,
@@ -21,3 +58,6 @@ gsap.to("#main", {
     scrub: 3,
   },
 });
+
+circleMouseFollow();
+circleSkew();
